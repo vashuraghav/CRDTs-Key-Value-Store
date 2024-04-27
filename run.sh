8c03f13d3be6ffbda60c8a5fa05bd52b10db83a1
vashuraghav <vashuraghav50@gmail.com>
2024-04-27 19:10:03 -0400

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change directory to your project root (adjust this if needed)
+make clean
+# Run CMake
+cmake .
+
+# Build the project
+make
+
+# Run the executable
+./Build/bin/restserver
